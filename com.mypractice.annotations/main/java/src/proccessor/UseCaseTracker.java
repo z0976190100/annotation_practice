9e23b3bae33b0fab771452516f0f3b5702162024
z0976190100 <z0976190100@gmail.com>
2018-05-24 00:30:39 +0300

    init commit

---
+++ com.mypractice.annotations/main/java/src/proccessor/UseCaseTracker.java
@@ -0,0 +1,26 @@
+package proccessor;
+
+import annotation.UseCase;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+public class UseCaseTracker {
+
+    public static void trackUseCases(List<Integer> useCases, Class<?> clazz){
+        for(Method method : clazz.getDeclaredMethods()){
+            UseCase useCase = method.getAnnotation(UseCase.class);
+            if(useCase != null){
+                System.out.println("Found UseCase: " + useCase.id() + " " + useCase.description());
+                useCases.remove(new Integer(useCase.id()));
+            }
+        }
+        if(!useCases.isEmpty()) {
+            System.err.println("WARN: missing cases:");
+            for (int i : useCases) {
+                System.err.println("#" + i);
+            }
+        }
+    }
+
+}
---
+++ com.mypractice.annotations/main/java/src/util/PassUtils.java
@@ -0,0 +1,24 @@
+package util;
+
+import annotation.UseCase;
+
+public class PassUtils {
+
+    @UseCase(id = 47, description = "case num 47")
+    public boolean validatePass(String pass) {
+
+
+        return true;
+    }
+
+
+    @UseCase(id = 49)
+    public String encryptPass1(String pass){
+        return pass;
+    }
+    @UseCase(id = 50)
+    public String encryptPass2(String pass){
+        return pass;
+    }
+
+}
